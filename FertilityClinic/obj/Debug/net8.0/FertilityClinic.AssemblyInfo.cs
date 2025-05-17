619fffd4251afa2da26c7976c48be1650ca55eae
phuthang4444 <thangtpse151058@fpt.edu.vn>
2025-05-17 12:50:44 +0700

    fix bug

--- FertilityClinic/obj/Debug/net8.0/FertilityClinic.AssemblyInfo.cs
+++ FertilityClinic/obj/Debug/net8.0/FertilityClinic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FertilityClinic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e421c18ff575ec3018d0b478fb8b8412bee6f823")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3423e4c13cea34dac734d286fa5056c45610673")]
 [assembly: System.Reflection.AssemblyProductAttribute("FertilityClinic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FertilityClinic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
