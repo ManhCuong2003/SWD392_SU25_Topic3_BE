a3423e4c13cea34dac734d286fa5056c45610673
phuthang4444 <thangtpse151058@fpt.edu.vn>
2025-05-17 12:15:28 +0700

    setup code base BE

---
+++ FertilityClinic.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FertilityClinic.DAL.Repositories.Implementations;
+
+namespace FertilityClinic.DAL.UnitOfWork
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : class;
+        Task<int> SaveAsync();
+    }
+}
