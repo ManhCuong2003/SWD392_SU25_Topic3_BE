8b716b5c794486013c6402f44a2ad113ee7f22ba
phuthang4444 <thangtpse151058@fpt.edu.vn>
2025-06-24 14:11:43 +0700

    fix: remove payment

--- FertilityClinic.DAL/Migrations/20250624070957_remove_payment.Designer.cs
+++ FertilityClinic.DAL/Migrations/20250624070957_remove_payment.Designer.cs
@@ -4,6 +4,7 @@ using FertilityClinic.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FertilityClinic.DAL.Migrations
 {
     [DbContext(typeof(FertilityClinicDbContext))]
-    partial class FertilityClinicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250624070957_remove_payment")]
+    partial class remove_payment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
