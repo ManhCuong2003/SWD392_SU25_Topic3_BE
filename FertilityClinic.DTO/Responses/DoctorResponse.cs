a6203b276cd5bfa8ba7886d3dec932b2484f38fe
phuthang4444 <thangtpse151058@fpt.edu.vn>
2025-06-28 17:23:46 +0700

    refactor: change doctor response and seed 2 account for admin and doctor

--- FertilityClinic.DTO/Responses/DoctorResponse.cs
+++ FertilityClinic.DTO/Responses/DoctorResponse.cs
@@ -11,10 +11,13 @@ namespace FertilityClinic.DTO.Responses
         public int DotorId { get; set; }
         public string DoctorName { get; set; } = null!;
         public string Email { get; set; } = null!;
-
         public string Specialization { get; set; } = null!;
         public string Degree { get; set; } = null!;
-        
+        public string Avatar { get; set; }
+        public List<string> Experience { get; set; } = new List<string>();
+        public int ExperienceYears { get; set; }
+        public string Bio { get; set; }
+        public List<string> Education { get; set; } = new List<string>();
 
     }
 }
