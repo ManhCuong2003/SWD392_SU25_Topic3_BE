eb6bf75e5b386301aedc0b9afdcba5aa7d43170e
phuthang4444 <thangtpse151058@fpt.edu.vn>
2025-06-28 16:21:14 +0700

    fix: add doctor

--- FertilityClinic.DAL/Migrations/20250628091911_change_certification_to_experience_in_doctor.Designer.cs
+++ FertilityClinic.DAL/Migrations/20250628091911_change_certification_to_experience_in_doctor.Designer.cs
@@ -4,6 +4,7 @@ using FertilityClinic.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FertilityClinic.DAL.Migrations
 {
     [DbContext(typeof(FertilityClinicDbContext))]
-    partial class FertilityClinicDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250628091911_change_certification_to_experience_in_doctor")]
+    partial class change_certification_to_experience_in_doctor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FertilityClinic.DAL/Migrations/20250628091911_change_certification_to_experience_in_doctor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FertilityClinic.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_certification_to_experience_in_doctor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Certifications",
+                table: "Doctors",
+                newName: "Experience");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Experience",
+                table: "Doctors",
+                newName: "Certifications");
+        }
+    }
+}
