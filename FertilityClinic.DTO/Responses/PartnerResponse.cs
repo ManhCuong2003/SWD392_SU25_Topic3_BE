ed589ee9e55770fad3c1c6a5145fe00a9be62af6
Cuong0306 <cuongnpse173621@fpt.edu.vn>
2025-07-13 15:40:03 +0700

    addResponsepartnerId

--- FertilityClinic.DTO/Responses/PartnerResponse.cs
+++ FertilityClinic.DTO/Responses/PartnerResponse.cs
@@ -8,6 +8,7 @@ namespace FertilityClinic.DTO.Responses
 {
     public class PartnerResponse
     {
+        public int? PartnerId { get; set; }
         public string FullName { get; set; }
         public DateOnly? DateOfBirth { get; set; }
         public string Gender { get; set; }
